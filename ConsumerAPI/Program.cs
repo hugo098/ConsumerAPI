c4b6cd96299523d2fb095fde16d2f765bafa35b4
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 10:02:41 -0400

    Configure swagger ui and automapper

--- ConsumerAPI/Program.cs
+++ ConsumerAPI/Program.cs
@@ -1,3 +1,9 @@
+using JSONPlaceholderConsumer.Services.IServices;
+using JSONPlaceholderConsumer.Services.JSONPlaceholder;
+using JSONPlaceholderConsumer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(options => {
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "JSON Placeholder Consumer API v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Hugo Rodriguez",
+            Url = new Uri("https://github.com/hugo098")
+        },
+    });
+});
+
+builder.Services.AddHttpClient<IJSONPlaceholderService, JsonPlaceholderPostService>();
+builder.Services.AddScoped<IJSONPlaceholderService, JsonPlaceholderPostService>();
+builder.Services.AddHttpClient<JsonPlaceholderUserService>();
+builder.Services.AddScoped<JsonPlaceholderUserService>();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 var app = builder.Build();
 
