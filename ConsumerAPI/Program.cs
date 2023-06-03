c1e544d4075bbe30be64ef60abca45c092511bb9
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 10:06:42 -0400

    Add swagger version endpoint

--- ConsumerAPI/Program.cs
+++ ConsumerAPI/Program.cs
@@ -57,7 +57,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ConsumerAPIv1");
+    });
 }
 
 app.UseHttpsRedirection();
