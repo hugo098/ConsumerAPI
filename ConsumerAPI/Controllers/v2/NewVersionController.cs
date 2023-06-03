f335f1df091a5a07afc440c460eb11be9dcd440c
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 11:54:06 -0400

    Add v2 controller

---
+++ ConsumerAPI/Controllers/v2/NewVersionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumerAPI.Controllers.v2
+{
+    [Route("api/v{version:apiVersion}/newversion")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    public class NewVersionController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<string> GetPosts()
+        {
+            return Ok("Hello world");
+
+        }
+    }
+}
