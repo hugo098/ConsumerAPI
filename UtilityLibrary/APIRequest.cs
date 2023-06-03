f19212886f2daaaa3766e44d2b5b06701d9d306c
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 09:49:25 -0400

    Add utility library classes and base service

---
+++ UtilityLibrary/APIRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UtilityLibrary
+{
+    public class APIRequest
+    {
+        public APIMethodType ApiMethodType { get; set; } = APIMethodType.GET;
+        public string? Url { get; set; }
+        public object? Data { get; set; }
+    }
+}
