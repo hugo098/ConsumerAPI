65db653b63383de5a34b0d2f0174d0b8801e3ab3
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 09:53:27 -0400

    Add JsonPlaceholder service implementation - Pots: getAll, getOne, create, update, delete - User: getOne

---
+++ ConsumerAPI/Services/JSONPlaceholder/JsonPlaceholderUserService.cs
@@ -0,0 +1,23 @@
+﻿using UtilityLibrary;
+
+namespace JSONPlaceholderConsumer.Services.JSONPlaceholder
+{
+    public class JsonPlaceholderUserService : BaseService
+    {
+        private string JSONPlaceholderURL;
+
+        public JsonPlaceholderUserService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {            
+            JSONPlaceholderURL = configuration.GetValue<string>("ServiceUrls:JSONPlaceholderAPI");
+        }
+
+        public async Task<T> GetAsync<T>(int id)
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiMethodType = APIMethodType.GET,
+                Url = JSONPlaceholderURL + "/users/" + id
+            });
+        }
+    }
+}
