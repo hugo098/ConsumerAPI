76fc0f2a115fb2de719b93b68b26d8b303b2820b
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-03 09:42:18 -0400

    Add post models and dtos

---
+++ ConsumerAPI/Models/Posts/Dtos/UpdatePostDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JSONPlaceholderConsumer.Models.Posts.Dtos
+{
+    public class UpdatePostDTO
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {0}")]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(100), MinLength(1)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(500), MinLength(1)]
+        public string Body { get; set; }
+    }
+}
